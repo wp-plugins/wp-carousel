fefd389ae2eccf0854bc0233158b818a129313e7
sumolari <sumolari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-27 15:32:56 +0000

    Enviando versión 0.3.0.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-carousel/tags/0.3.0.4@149659 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/es.php
@@ -0,0 +1,47 @@
+<?php
+	$wp_carousel_txt = array(
+		
+		1 => "WP Carousel - Opciones Generales",
+		2 => "&iquest;Tienes alg&uacute;n problema? Encontrar&aacute;s tutoriales en <a href=\"http://sumolari.com/plugins-temas/wp-carousel/#spanish-tutorials\">esta p&aacute;gina</a>.",
+		3 => "Idioma",
+		4 => "English",
+		5 => "Espa&ntilde;ol",
+		6 => "Carruseles",
+		7 => "Guardar Cambios",
+		
+		8 => "Carrusel",
+		
+		9 => "<strong>Nota:</strong> Para a&ntilde;adir el Carrusel, a&ntilde;ade",
+		10 => "en tu theme.",
+		11 => "Puedes editar las opciones de WP Carousel aqu&iacute;.",
+		12 => "Opciones del Carrusel",
+		13 => "ID de la categor&iacute;a de los art&iacute;culos",
+		14 => "N&uacute;mero de art&iacute;culos a mostrar",
+		15 => "Texto para el link \"Leer m&aacute;s\" ",
+		16 => "&iquest;Mostrar primero los art&iacute;culos o el Contenido Personalizado?",
+		17 => "Art&iacute;culos",
+		18 => "Contenido Personalizado",
+		19 => "Mostrar/Ocultar opciones avanzadas",
+		20 => "&iquest;Desplazamiento autom&aacute;tico?",
+		21 => "S&iacute;",
+		22 => "Tiempo entre cada desplazamiento autom&aacute;tico (en ms)",
+		23 => "N&uacute;mero de art&iacute;culos a desplazar en cada desplazamiento autom&aacute;tico",
+		24 => "N&uacute;mero de art&iacute;culos a desplazar en cada desplazamiento manual",
+		25 => "Opciones de Dise&ntilde;o",
+		26 => "Ancho del contenedor de los art&iacute;culos del Carrusel (<strong>primero</strong>, ancho; <strong>segundo</strong>, unidad)",
+		27 => "&iquest;Usar la misma anchura para las im&aacute;genes de los art&iacute;culos y para sus contenedores?",
+		29 => "Altura y unidad (d&eacute;jalo en blanco para usar la recomendada)",
+		30 => "A&ntilde;ade m&aacute;s contenido",
+		31 => "URL de la imagen",
+		32 => "URL de link",
+		33 => "Texto",
+		34 => "Orden",
+		35 => "A&ntilde;ade m&aacute;s Contenido Personalizado",
+		36 => "(deja el campo en blanco para eliminar el Contenido Personalizado)",
+		
+		37 => "&iquest;D&oacute;nde mostrar el Carrrusel?",
+		
+		38 => "ID del Carrusel"
+		
+	);
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Plugin Name ===
+Tags: posts, images, carousel, theme, sidebar, widget
+Requires at least: 2.8
+Tested up to: 2.8.4
+Stable tag: 0.3.0.4
+
+WP Carousel is a plugin that allows you to add a carousel with a category's posts. Is easy to install and use.
+
+== Description ==
+
+WP Carousel is a plugin that create a carousel with a category's posts, and you can put it anywhere on the blog.
+
+In the plugin's options page you can select the posts's category and the number of posts to show.
+
+The plugin uses jQuery, and load it directly from your WordPress installation files, so if you have a WordPress version without jQuery, it won't work.
+
+== Installation ==
+
+1. Upload `wp-carousel` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php wp_carousel(CAROUSEL'S ID); ?>` in your templates
+
+More info in: http://sumolari.com/plugins-temas/wp-carousel/#english-tutorials
+
+== Frequently Asked Questions ==
+
+= How can I edit the carousel's style =
+
+You can edit the carousel's images placed in `img` folder (inside `wp-carousel` folder).
+
+CSS code is located in `carousel-css.php`, in `wp-carousel`.
+
+== Demo ==
+
+You can see a demo in: http://sumolari.com/demos/wp-carousel
