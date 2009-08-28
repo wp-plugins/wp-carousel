47e5c5785fcd52962d1ec1c99bc7e67589cf3ff3
sumolari <sumolari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-28 07:29:58 +0000

    Enviando versión 0.3.0.4.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-carousel/tags/0.3.0.4.1@149925 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ carousel-css.php
@@ -0,0 +1,145 @@
+<?php
+	$path_1 = plugin_basename(__FILE__);
+	
+	$path_2 = ereg_replace('/carousel-css.php', '', $path_1);
+	
+	/* Pre 2.6 */
+	
+	if (!defined( 'WP_CONTENT_URL' ) )
+		define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+	if (!defined( 'WP_PLUGIN_URL' ) )
+		define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+		
+	$wp_carousel_options_serialized = get_option('wp_carousel_options');
+	
+	$wp_carousel_options = unserialize($wp_carousel_options_serialized);
+					
+	$panel_width = $wp_carousel_options['carousel_1']['panel_width'];
+	$panel_width_unit = $wp_carousel_options['carousel_1']['panel_width_unit'];
+					
+	$panel_height = $wp_carousel_options['carousel_1']['image_more_height_h'];
+	$panel_height_unit = $wp_carousel_options['carousel_1']['image_more_height_h_unit'];
+	
+	$wp_carousel_carousels = $wp_carousel_options['carousels'];
+		
+?>
+/* Carousel de artículos destacados */
+					
+			.wp_carousel {
+				position: relative; /* No edit */
+				margin:10px 0 10px 0;
+				min-height: 50px;
+				background:#5B5B5B url(<?php echo '', WP_PLUGIN_URL ,'/'.$path_2.'/'; ?>img/carousel-bg.png) bottom left repeat-x;
+			}
+			
+			.wp_carousel .belt {
+				position: absolute; /* No edit */
+				left: 0;
+				top: 0;
+			}
+			
+			.wp_carousel .panel {
+				float: left; /* No edit */
+				overflow: hidden; /* No edit */
+				margin:4px;
+				padding:7px;
+				border:1px solid #5B5B5B;
+				background:#383838 url(<?php echo '', WP_PLUGIN_URL ,'/'.$path_2.'/'; ?>img/carousel-panel-bg.png) bottom left repeat-x;
+				-moz-border-radius:5px;
+			}
+			
+			.wp_carousel .panel .panel-text {
+				padding-top:5px;
+				font-size:13px;
+				font-family:Verdana, Geneva, sans-serif;
+				color:#FFF;
+			}
+			
+			.wp_carousel .panel .panel-text a {
+				color:#CCC;
+				text-decoration:none;
+			}
+			
+			.wp_carousel .panel .panel-text a:hover {
+				color:#FFF;
+				text-decoration:underline;
+			}
+			
+						/* Botones del carousel */
+						
+			.button-prev {
+				margin:10px 0 10px 0;
+				min-height:50px;
+				width:35px;
+				float:left;
+				background:#5B5B5B url(<?php echo '', WP_PLUGIN_URL ,'/'.$path_2.'/'; ?>img/carousel-bg.png) bottom left repeat-x;
+				-moz-border-radius:10px 0 0 10px;
+			}
+			
+			.button-prev a {
+				display:block;
+				padding:5px;
+			}
+			
+			.button-next {
+				margin:10px 0 10px 0;
+				min-height:50px;
+				width:35px;
+				float:right;
+				background:#5B5B5B url(<?php echo '', WP_PLUGIN_URL ,'/'.$path_2.'/'; ?>img/carousel-bg.png) bottom left repeat-x;
+				-moz-border-radius:0 10px 10px 0;
+			}
+			
+			.button-next a {
+				display:block;
+				padding:5px;
+			}
+			
+					/* Widget de Carrusel */
+					
+			.widget_wp_carousel_widget .wp_carousel .panel {
+				width:96px;
+			}
+			
+			.widget_wp_carousel_widget .button-prev, .widget_wp_carousel_widget .button-next {
+				margin:0px 0 0 0;
+			}
+			
+					/* Estilos únicos de cada carrusel */			
+					
+<?php
+		
+	$n = 0;
+	while ($wp_carousel_carousels > $n) {
+		$n++;
+?>
+		
+		#wp_carousel_<?php echo $n; ?>.wp_carousel.carousel, #wp_carousel_<?php echo $n; ?>.wp_carousel.carousel .belt, .button-next.carousel_<?php echo $n; ?>, .button-prev.carousel_<?php echo $n; ?> {
+			<?php 
+			if($panel_height != '') {
+				?>
+				height: <?php if ($panel_height_unit == 'px') { echo $panel_height+25; } else { echo $panel_height+3; } ?><?php if ($panel_height_unit == 'px') { echo 'px'; } elseif ($panel_height_unit == 'percent') { echo '%'; } else { echo 'px'; } echo ';';
+			} else { echo 'height: 250px;'; } ?>
+		}
+		
+		.button-prev.carousel_<?php echo $n; ?> a, .button-next.carousel_<?php echo $n; ?> a {
+			margin-top: <?php if ($panel_height != '') { $ph = $panel_height - 35; $ph = $ph / 2; echo $ph; } else { echo '105'; } ?>px;
+		}
+		
+		#wp_carousel_<?php echo $n; ?>.wp_carousel.carousel .belt .panel {
+			<?php 
+			if($panel_height != '') {
+				?>
+				height: <?php echo $panel_height; ?><?php if ($panel_height_unit == 'px') { echo 'px'; } elseif ($panel_height_unit == 'percent') { echo '%'; } else { echo 'px'; } echo ';';
+			} else { echo 'height: 250px;'; } 
+			
+			if($panel_width != '') { ?>
+			width: <?php echo $panel_width; ?><?php if ($panel_width_unit == 'px') { echo 'px'; } elseif ($panel_width_unit == 'percent') { echo '%'; } else { echo 'px'; } echo ';';
+			} else { 
+				echo 'width: 360px;'; 
+			} ?>
+		}
+		
+<?php
+	}
+?>
---
+++ lang/en.php
@@ -0,0 +1,47 @@
+<?php
+	$wp_carousel_txt = array(
+		
+		1 => "WP Carousel's general options",
+		2 => "Do you have any problem? You can find the tutorials in <a href=\"http://sumolari.com/plugins-temas/wp-carousel/#english-tutorials\">this page</a>.",
+		3 => "Language",
+		4 => "English",
+		5 => "Espa&ntilde;ol",
+		6 => "Carousels",
+		7 => "Save Changes",
+		
+		8 => "Carousel",
+		
+		9 => "<strong>Note:</strong> To add the Carousel, add",
+		10 => "to your theme.",
+		11 => "You can edit WP Carousel's options here.",
+		12 => "Carousel Options",
+		13 => "Carousel posts's category ID",
+		14 => "Number of posts to display",
+		15 => "Text for the \"Read More\" links",
+		16 => "Show first posts or costumized content?",
+		17 => "Posts",
+		18 => "Costumized content",
+		19 => "Show/Hide Advanced Options",
+		20 => "Slide automatically?",
+		21 => "Yes",
+		22 => "Time between automatic slide (in ms)",
+		23 => "Number of posts to slide in each automatic slide",
+		24 => "Number of posts to slide in each manual slide",
+		25 => "Design options",
+		26 => "Carousel posts's panel width (<strong>first</strong>, width; <strong>second</strong>, unit)",
+		27 => "Use the same width for posts's images and posts's panel?",
+		29 => "Height and unit (leave blank to use the default one)",
+		30 => "Add more content",
+		31 => "Image URL",
+		32 => "Link URL",
+		33 => "Text",
+		34 => "Order",
+		35 => "Add more costumized content",
+		36 => "(erase the fields to delete custom content)",
+		
+		37 => "Where to show the Carousel?",
+		
+		38 => "Carousel's ID"
+		
+	);
+?>
